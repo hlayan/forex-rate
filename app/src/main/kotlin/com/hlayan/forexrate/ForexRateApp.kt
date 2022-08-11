af72647044e6e957b2336fd393b40440c33e7514
Hlayan Htet Aung <hlayanhtetaung@gmail.com>
2022-08-11 16:36:05 +0630

    Change: 'MyForexApp' to 'ForexRateApp'

--- app/src/main/kotlin/com/hlayan/forexrate/ForexRateApp.kt
+++ app/src/main/kotlin/com/hlayan/forexrate/ForexRateApp.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class MyForexApp : Application() {
+class ForexRateApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
