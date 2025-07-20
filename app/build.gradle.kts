2f52929b7add7353c9582992eb21186b39653841
Hlayan Htet Aung <hlayanhtetaung@gmail.com>
2025-07-20 21:37:47 +0700

    Release v1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     defaultConfig {
         minSdk = 21
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
         applicationId = "com.hlayan.forexrate"
         vectorDrawables.useSupportLibrary = true
     }
